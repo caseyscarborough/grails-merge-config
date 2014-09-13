afac5c7e9d38254cdf9f4a84733b22a1791073a5
Casey Scarborough <caseyscarborough@gmail.com>
2014-09-13 09:04:56 -0400

    Update plugin descriptor.

--- MergeConfigGrailsPlugin.groovy
+++ MergeConfigGrailsPlugin.groovy
@@ -8,62 +8,22 @@ class MergeConfigGrailsPlugin {
       "grails-app/views/error.gsp"
   ]
 
-  // TODO Fill in these fields
   def title = "Merge Config Plugin" // Headline display name of the plugin
-  def author = "Your name"
-  def authorEmail = ""
+  def author = "Casey Scarborough"
+  def authorEmail = "caseyscarborough@gmail.com"
   def description = '''\
-Brief summary/description of the plugin.
+This plugin gives users the ability to manage their Configuration options, typically
+held in Config.groovy, from within the application without requiring a restart.
 '''
 
   // URL to the plugin's documentation
-  def documentation = "http://grails.org/plugin/merge-config"
+  def documentation = "https://github.com/caseyscarborough/grails-merge-config"
 
-  // Extra (optional) plugin metadata
-
-  // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-  // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-  // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+  def license = "MIT"
 
   // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+  def issueManagement = [ system: "GitHub", url: "https://github.com/caseyscarborough/grails-merge-config/issues" ]
 
   // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-  def doWithWebDescriptor = { xml ->
-    // TODO Implement additions to web.xml (optional), this event occurs before
-  }
-
-  def doWithSpring = {
-    // TODO Implement runtime spring config (optional)
-  }
-
-  def doWithDynamicMethods = { ctx ->
-    // TODO Implement registering dynamic methods to classes (optional)
-  }
-
-  def doWithApplicationContext = { ctx ->
-    // TODO Implement post initialization spring config (optional)
-  }
-
-  def onChange = { event ->
-    // TODO Implement code that is executed when any artefact that this plugin is
-    // watching is modified and reloaded. The event contains: event.source,
-    // event.application, event.manager, event.ctx, and event.plugin.
-  }
-
-  def onConfigChange = { event ->
-    // TODO Implement code that is executed when the project configuration changes.
-    // The event is the same as for 'onChange'.
-  }
-
-  def onShutdown = { event ->
-    // TODO Implement code that is executed when the application shuts down (optional)
-  }
+  def scm = [ url: "https://github.com/caseyscarborough/grails-merge-config" ]
 }
