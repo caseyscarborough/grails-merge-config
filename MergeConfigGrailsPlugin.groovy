e96f4480327540d5d2d94ff28f51ffea75d3fc80
Casey Scarborough <caseyscarborough@gmail.com>
2014-09-13 16:34:46 -0400

    Update plugin descriptor.

--- MergeConfigGrailsPlugin.groovy
+++ MergeConfigGrailsPlugin.groovy
@@ -1,16 +1,11 @@
-import grails.plugin.mergeconfig.Configuration
-
 class MergeConfigGrailsPlugin {
-  // the plugin version
   def version = "0.1"
-  // the version or versions of Grails the plugin is designed for
-  def grailsVersion = "2.4 > *"
-  // resources that are excluded from plugin packaging
+  def grailsVersion = "2.3 > *"
   def pluginExcludes = [
       "grails-app/views/error.gsp"
   ]
 
-  def title = "Merge Config Plugin" // Headline display name of the plugin
+  def title = "Merge Config Plugin"
   def author = "Casey Scarborough"
   def authorEmail = "caseyscarborough@gmail.com"
   def description = '''\
@@ -18,14 +13,8 @@ This plugin gives users the ability to manage their Configuration options, typic
 held in Config.groovy, from within the application without requiring a restart.
 '''
 
-  // URL to the plugin's documentation
   def documentation = "https://github.com/caseyscarborough/grails-merge-config"
-
   def license = "MIT"
-
-  // Location of the plugin's issue tracker.
   def issueManagement = [ system: "GitHub", url: "https://github.com/caseyscarborough/grails-merge-config/issues" ]
-
-  // Online location of the plugin's browseable source code.
   def scm = [ url: "https://github.com/caseyscarborough/grails-merge-config" ]
 }
